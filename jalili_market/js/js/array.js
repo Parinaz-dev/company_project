ca0e08ca55c14acfd84d8c78655a0a22ea1e37ac
parinaz <p.ebrahimi520@gmail.com>
2024-02-05 06:48:22 -0800

    added files in project

---
+++ jalili_market/js/js/array.js
@@ -0,0 +1,4 @@
+var family_persons = ["parinaz", "setayesh", "roya"];
+var nameric = [21, 18, 19];
+console.log(family_persons);
+console.log(nameric);
---
+++ jalili_market/js/js/shey.js
@@ -0,0 +1,15 @@
+var family = [{
+        name: 'parinaz',
+        last_name: 'Ebrahimi',
+        favorite_color: 'black',
+        age: 21,
+        weight: 50
+    },
+    {
+        name: 'parisa',
+        maried: false,
+        favorite_food: "soap"
+    }
+]
+
+console.log(family);
